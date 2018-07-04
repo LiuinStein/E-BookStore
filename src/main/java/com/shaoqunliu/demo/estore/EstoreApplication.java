58cb2d47e5c5bab71f0a81b419b1104220031f5d
Shaoqun Liu <liuinstein@163.com>
2018-07-04 14:20:49 +0800

    Initial commit

---
+++ src/main/java/com/shaoqunliu/demo/estore/EstoreApplication.java
@@ -0,0 +1,12 @@
+package com.shaoqunliu.demo.estore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstoreApplication.class, args);
+    }
+}
---
+++ src/test/java/com/shaoqunliu/demo/estore/EstoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shaoqunliu.demo.estore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EstoreApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
